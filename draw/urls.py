1d55692d6aae6d81b51b0103d27371ef940a9f74
511895786 <511895786@qq.com>
2018-07-15 19:59:03 -0400

    commit message

--- draw/urls.py
+++ draw/urls.py
@@ -3,8 +3,13 @@ from django.conf.urls import url
 
 from . import views
 
+from django.urls import path
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    
+#     path('fitting/',views.c, name='fitting'),
+    url(r'^fitting/$', views.fitting, name='fitting')
 ]
 
--- draw/views.py
+++ draw/views.py
@@ -5,6 +5,9 @@ import json
 def index(request):
     return render(request, 'draw/index.html', {})
 
+def fitting(request):
+	return render(request, 'draw/fitting.html', {})
+  
 def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
